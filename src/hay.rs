e1aadd8987b28d0cd9715a7c9c9ef60016359624
Andy Farrell <git@andyf.me>
2023-06-27 12:59:44 +0100

    Improve char-index mapping

--- src/hay.rs
+++ src/hay.rs
@@ -5,56 +5,45 @@ use std::{
 };
 use rand::{thread_rng, Rng};
 
-const COMMON_EXTRA_CHARS: &str = "–—“”‘’£œæéèêĊøż×";
 const EXTRA_CHARS_BASE_IDX: usize = 129;
 
 struct CharIdxMap {
     char_to_idx: HashMap<char, usize>,
     idx_to_char: HashMap<usize, char>,
-    extra_chars_next_idx: usize,
 }
 
-// This handles mapping frequency table indices for non-ASCII characters
+// This handles mapping frequency table indices for ASCII and non-ASCII characters
 impl CharIdxMap {
-    fn new() -> Self {
+    fn new(input: &str) -> Self {
         let mut result = Self {
             char_to_idx: HashMap::new(),
             idx_to_char: HashMap::new(),
-            extra_chars_next_idx: EXTRA_CHARS_BASE_IDX,
         };
 
-        for (idx, c) in COMMON_EXTRA_CHARS.chars().enumerate() {
-            result.char_to_idx.insert(c, idx + EXTRA_CHARS_BASE_IDX);
-            result.idx_to_char.insert(idx + EXTRA_CHARS_BASE_IDX, c);
+        let mut non_ascii_idx = EXTRA_CHARS_BASE_IDX;
+        for c in input.chars() {
+            if c.is_ascii() { continue; }
+            if result.char_to_idx.get(&c).is_none() {
+                result.char_to_idx.insert(c, non_ascii_idx);
+                result.idx_to_char.insert(non_ascii_idx, c);
+                non_ascii_idx += 1;
+            }
         }
 
         result
     }
 
-    fn index_of(&mut self, c: char) -> Option<usize> {
+    fn index_for(&self, c: char) -> usize {
         if c.is_ascii() {
-            let code = c as usize;
-            if code == 9 || code == 10 || code == 13 || code >= 32 {
-                return Some(code)
-            } else {
-                return None
-            }
-        }
-
-        match self.char_to_idx.get(&c) {
-            Some(&idx) => Some(idx),
-            None => {
-                self. extra_chars_next_idx += 1;
-                self.char_to_idx.insert(c, self.extra_chars_next_idx);
-                self.idx_to_char.insert(self.extra_chars_next_idx, c);
-                Some(self.extra_chars_next_idx)
-            }
+            c as usize
+        } else {
+            *self.char_to_idx.get(&c).unwrap()
         }
     }
 
     fn char_at(&self, idx: usize) -> char {
         if idx > 128 {
-            *self.idx_to_char.get(&idx).unwrap_or(&'?')
+            *self.idx_to_char.get(&idx).unwrap_or(&char::REPLACEMENT_CHARACTER)
         } else {
             unsafe { char::from_u32_unchecked(idx as u32) }
         }
@@ -79,26 +68,30 @@ impl Hay {
         let mut output = String::with_capacity(count);
 
         let mut current_len: usize = 0;
-        let mut char_idx_map = CharIdxMap::new();
+        let char_idx_map = CharIdxMap::new(input);
         let mut freq_table: Vec<usize> = vec![0; 256]; // ASCII plus some room for some common
 
         let mut rng = thread_rng();
         let initial_seq_start: usize = rng.gen_range(0..input.chars().count() - seq_len);
-        output.push_str(&Hay::get_range(input, initial_seq_start, seq_len));
+        let initial_str: String = Hay::get_range(input, initial_seq_start, seq_len);
+        output.push_str(&initial_str);
 
 
         while current_len < count {
             freq_table.fill(0);
             let seq = Hay::get_seq(&output, seq_len);
 
+            // Count frequencies of characters that follow matches.
             for (idx, _) in input.match_indices(&seq) {
-                let slice = &input[idx..];
-                let Some(next_char) = slice.chars().nth(seq_len) else { continue };
-                let Some(table_idx) = char_idx_map.index_of(next_char) else { continue };
+                let Some(next_char) = input[idx..].chars().nth(seq_len) else { continue };
+                let table_idx = char_idx_map.index_for(next_char);
                 freq_table[table_idx] += 1;
             }
 
             let sum: usize = freq_table.iter().sum();
+
+            // This can happen when there is only a single instance of a character in the whole
+            // input.
             if sum == 0 {
                 if output.chars().count() > seq_len {
                     output.pop();
@@ -108,23 +101,23 @@ impl Hay {
                 }
             }
 
+            // Pick a random next character, biased by frequency.
             let mut n: usize = max(1, rng.gen_range(0..sum));
-            if let Some(idx) = freq_table.iter().enumerate().find_map(|(i, &freq)| {
+            let mut idx = 0;
+            for &freq in freq_table.iter() {
                 n = n.saturating_sub(freq);
                 if n == 0 {
-                    Some(i)
-                } else {
-                    None
+                    break
                 }
-            }) {
-                let new_char = char_idx_map.char_at(idx);
-                output.push(new_char);
-                if live {
-                    print!("{new_char}");
-                    let _ = io::stdout().flush();
-                }
-                current_len += 1;
+                idx += 1;
             }
+            let new_char = char_idx_map.char_at(idx);
+            output.push(new_char);
+            if live {
+                print!("{new_char}");
+                let _ = io::stdout().flush();
+            }
+            current_len += 1;
         }
 
         output
